3f7736f89e2d693653729728874104d69742d0b4
nitzantomer <nitzan.tomer@gmail.com>
2018-02-15 16:56:03 +0200

    webpack and stylus

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
-const {getIfUtils, removeEmpty} = require("webpack-config-utils");
+const { getIfUtils, removeEmpty } = require("webpack-config-utils");
 
 // variables
 const outPath = path.join(__dirname);
@@ -8,11 +8,12 @@ const htmlPath = path.join(__dirname, "html");
 
 // plugins
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+//const stylus_plugin = require('stylus_plugin');
 
-const htmlTemplate = path.join(htmlPath, "dev-template.html");
+const htmlTemplate = path.join(htmlPath, "index.html");
 
 module.exports = env => {
-	const {ifNotProd} = getIfUtils(env || {});
+	const { ifNotProd } = getIfUtils(env || {});
 	// get boolean value to use directly in flag configuration;
 	const isNotProd = ifNotProd(true, false);
 
@@ -28,12 +29,17 @@ module.exports = env => {
 		},
 		module: {
 			noParse: [/\.min\.js$/, /\.bundle\.js$/],
-			rules: []
+			rules: [{
+				test: /\.styl$/,
+				use: [
+					"style-loader",
+					"css-loader",
+					"stylus-loader"
+				]
+			}]
 		},
 		resolve: {
 			extensions: [".ts", ".js"],
-			// Fix webpack"s default behavior to not load packages with jsnext:main module
-			// (jsnext:main directs not usually distributable es6 format, but es6 sources)
 			mainFields: ["module", "browser", "main"]
 		},
 		target: "web",
