af460cd5c66e9df9fbc1c223ad595d87f3c7abd4
Lior Aviram <lior.aviram@gmail.com>
2018-03-20 12:41:14 +0200

    ECO-130 iOS bridge adaption

--- scripts/src/bridge.ts
+++ scripts/src/bridge.ts
@@ -1,27 +1,46 @@
+/****
+	KinNative:
+    loaded(): void;
+    handleResult(data: any): void;
+    handleCancel(): void;
+****/
+
 declare var KinNative: {
-	loaded(): void;
-	handleResult(data: any): void;
-	handleCancel(): void;
+	[key: string]: (...data: any[]) => void;
 };
-
-export function notifyPageLoaded() {
-	KinNative.loaded();
-}
-
-export function submitResult(data: any) {
-	console.log ("Submitting data:", data);
-	KinNative.handleResult(JSON.stringify(data));
-}
-
-export function close() {
-	console.log ("Requesting to close");
-	KinNative.handleCancel();
-}
 
 declare global {
 	interface Window {
 		kin: { renderPoll: (data: any) => void };
+		webkit: any;
 	}
+}
+
+
+function callNativeMethod(methodName: string, ...payload: any[]) {
+	console.log("calling method", methodName);
+	if (KinNative && methodName in KinNative) {
+		return KinNative[ methodName ](...payload);
+	}
+	if (window.webkit && window.webkit.messageHandlers && methodName in window.webkit.messageHandlers) {
+		return window.webkit.messageHandlers[ methodName ].postMessage(payload);
+	}
+	throw new Error("Couldn't find a native bridge interface");
+}
+
+export function notifyPageLoaded() {
+	console.log("loaded");
+	callNativeMethod("loaded");
+}
+
+export function submitResult(data: any) {
+	console.log("Submitting data:", data);
+	callNativeMethod("handleResult", JSON.stringify(data));
+}
+
+export function close() {
+	console.log("Requesting to close");
+	callNativeMethod("handleCancel");
 }
 
 window.kin = window.kin || {};
