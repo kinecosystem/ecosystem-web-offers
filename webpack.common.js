0eaddfa3e6da2e72bfcfaa4a8a7b5d5b3bab0e30
Lior Aviram <lior.aviram@gmail.com>
2018-05-15 17:45:00 +0300

    ECO-233 Seprate prod/dev builds and S3 NPM deploy scripts

---
+++ webpack.common.js
@@ -0,0 +1,50 @@
+const path = require("path");
+// variables
+const outPath = path.join(__dirname);
+const htmlPath = path.join(__dirname, "html");
+
+// plugins
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const htmlTemplate = path.join(htmlPath, "index.html");
+
+module.exports = {
+		entry: [ path.join(__dirname, "scripts", "src", "index.tsx")],
+		target: "web",
+		output: {
+			path: outPath,
+			filename: "[name].bundle.js",
+			publicPath: "/"
+		},
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					use: 'ts-loader',
+					exclude: /node_modules/
+				},
+				{
+					test: /\.styl$/,
+					use: [
+						"style-loader",
+						"css-loader",
+						"stylus-loader"
+					]
+				}, {
+					test: /\.css$/,
+					use: [
+						"style-loader",
+						"css-loader",
+					]
+				}]
+		},
+		resolve: {
+			extensions: [".ts", ".tsx", ".js"],
+			mainFields: ["module", "browser", "main"]
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: htmlTemplate,
+			}),
+		]
+	};
