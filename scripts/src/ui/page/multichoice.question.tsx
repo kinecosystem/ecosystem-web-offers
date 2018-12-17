kinecosystem/ecosystem-web-offers

commit 603d5423517db347d169ed8b9902912ee39b3b46

Submitting data works with answers indices, not the answers full texts

scripts/src/ui/page/multichoice.question.tsx
@@ -80,12 +80,12 @@ export class MultichoiceQuestion<P extends FullPageMultiChoiceProps = FullPageMu
 		});
 	}
 
-	protected onSelect(value: string, index: number) {
+	protected onSelect(index: number) {
 		const props = this.props;
 		const sharedData = props.sharedData;
 		const oneBasedIndex = index + 1;
 		const isRightAnswer = oneBasedIndex === props.rightAnswer;
-		const results = { [ this.props.id ]: value };
+		const results = { [ this.props.id ]: oneBasedIndex };
 		const newSharedData: SharedData = isRightAnswer ? {} : { wrongAnswers: Object.assign({}, sharedData.wrongAnswers, results) };
 		let delay = 0;  // Delay between selected answer and transition to next page
 

html/index.html
@@ -100,7 +100,7 @@
 									amount: 15,
 									rightAnswer: 2,
 									question: {
-										id: "wrtwrthwrhr",
+										id: "question-1",
 										answers: [
 											"Not a lot, they are very small",
 											"A woodchuck would chuck as much wood as a woodchuck could chuck",
@@ -115,7 +115,7 @@
 									amount: 20,
 									rightAnswer: 3,
 									question: {
-										id: "eee",
+										id: "question-2",
 										answers: [
 											"Of course",
 											"no",
@@ -130,7 +130,7 @@
 									amount: 15,
 									rightAnswer: 4,
 									question: {
-										id: "eee",
+										id: "question-3",
 										answers: [
 											"Yellow",
 											"Red",

scripts/src/app.tsx
@@ -36,7 +36,7 @@ export enum PageType {
 export interface SharedData {
 	wrongAnswers?: {};
 	earnedAmount?: number;
-};
+}
 
 export interface CommonProps {
 	key: number;

scripts/src/bridge.ts
@@ -19,7 +19,6 @@ declare global {
 	}
 }
 
-
 function callNativeMethod(methodName: string, ...payload: any[]) {
 	console.log("calling method", methodName);
 	if (window.KinNative && methodName in KinNative) {

scripts/src/ui/container/carousel.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import  SwipeableViews, { SwipeableViewsProps } from 'react-swipeable-views';
+import SwipeableViews, { SwipeableViewsProps } from "react-swipeable-views";
 
 const carouselSettings: any = {
 	disabled: true,

scripts/src/ui/page/earnThankYou.tsx
@@ -6,8 +6,8 @@ import "../../../../styles/src/page/earnThankYou.styl";
 
 export interface EarnThankYouProps extends CommonProps {
 	amount: number;
-	closeHandler(answerData: any): void;
 	isDisplayed: boolean;  // is this the currently displayed page
+	closeHandler(answerData: any): void;
 	hideTopBarHandler(): void;
 }
 

scripts/src/ui/page/timed.multichoice.question.tsx
@@ -77,7 +77,7 @@ class TimedMultichoiceQuestion extends MultichoiceQuestion {
 	protected getButtons() {
 		return this.props.choices.map((value: any, index: number) => {
 			const additionalClasses = this.getAdditionalButtonClasses(value, index);
-			const clickCb = this.onSelect.bind(this, value, index);
+			const clickCb = this.onSelect.bind(this, index);
 			return (
 				<button key={index} className={"choice " + additionalClasses} disabled={this.state.disabled} onClick={clickCb}>
 					{value}

commit 02ea16e8861da70962cd63c1a5f2ce61a1c0e21c

ECO-873 fix regression bug introduced by 603d542 (PR #14)

scripts/src/ui/page/multichoice.question.tsx
@@ -76,7 +76,7 @@ export class MultichoiceQuestion<P extends FullPageMultiChoiceProps = FullPageMu
 	protected getButtons() {
 		return this.props.choices.map((value, index) => {
 			const additionalClasses = this.getAdditionalButtonClasses(value, index);
-			return <button key={index} className={"choice " + additionalClasses} disabled={this.state.disabled} onClick={this.onSelect.bind(this, value, index)}>{value}</button>;
+			return <button key={index} className={"choice " + additionalClasses} disabled={this.state.disabled} onClick={this.onSelect.bind(this, index)}>{value}</button>;
 		});
 	}
 

webpack.dev.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
 		headers: {'Access-Control-Allow-Origin': '*'},
 		overlay: true,
 	},
-	plugins: [new NgrockWebpackPlugin({
-	   subdomain: "liorama"
-	})]
+	// plugins: [new NgrockWebpackPlugin({
+	//    subdomain: "liorama"
+	// })]
 });