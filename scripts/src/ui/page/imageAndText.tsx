f60e9fdba483a9a4ae293353df9618c5b850e8f5
Victor Sulema <victorsulema@Victors-MacBook-Pro.local>
2019-05-07 17:01:15 +0300

    tutorial page naviagation preparations

--- scripts/src/ui/page/imageAndText.tsx
+++ scripts/src/ui/page/imageAndText.tsx
@@ -1,18 +1,23 @@
 import * as React from "react";
 import BasePage from "./base";
+import { CommonProps } from "../../app";
 
 import "../../../../styles/src/page/imageAndText.styl";
 
-export interface ImageAndTextProps {
+export interface ImageAndTextProps extends CommonProps {
 	image: string;
-	title: string;
-	onBtnClick?: () => void;
-	btnText: string;
+	onBtnClick: (answerData: any) => void;
 	footerHtml: string;
+	bodyHtml: string;
+	buttonText: string;
 }
 
-class ImageAndText extends React.Component<any, ImageAndTextProps> {
+class ImageAndText extends React.Component<ImageAndTextProps> {
 	public render() {
+		const currentPage = this.props.pageIndex + 1;
+		/* the list is 0 based */
+		const totalQuestionPages = this.props.totalPagesCount - 1;
+		/* the list contains a Thank you page --- a better solution is better */
 		return (<BasePage className="imageAndText">
 			<div className="header">
 				<img src={this.props.image}/>
