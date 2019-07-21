60d6ea36a5d0774d2f586d0fc8e49fddddff0d65
Shahar Solomianik <shaharsol@Shahars-MacBook-Pro.local>
2019-07-21 13:10:49 +0300

    removed i18n

--- scripts/src/index.tsx
+++ scripts/src/index.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./app";
 import "../../styles/src/index.styl";
-import "./i18n";
 
 ReactDOM.render(
 	<App/>,
